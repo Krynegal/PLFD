dcfba6d670b73e105582ea3cbbdb76c04a096a20
Egor <pokalo.egor@mail.ru>
2023-09-25 13:44:25 +0300

    проверил, один комментарий

--- homework_1/task_5.py
+++ homework_1/task_5.py
@@ -4,3 +4,5 @@ m = -100
 n = 100
 
 print(True if m <= (-b // a) <= n else False)
+#todo можно не писать True, а оставить просто if(само условие, еслии верное, выдаст True, а если нет, то False)
+# print(m <= (-b // a) <= n)
