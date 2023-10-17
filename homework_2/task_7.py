e0113c51930bc5ece297dc53241405c2394d0a7e
IvanShtyrov <144477307+IvanShtyrov@users.noreply.github.com>
2023-10-17 20:50:22 +0300

    Update task_7.py

--- homework_2/task_7.py
+++ homework_2/task_7.py
@@ -1,3 +1,6 @@
+# Вместо двух циклов for для ввода размера коньков и ног людей, можно использовать один цикл и запрашивать данные поочередно
+# Можно избавиться от сортировки списков a и b, так как она не обязательна в данной задаче. Просто проходи по списку "ног" и коньков в порядке ввода
+
 a, b = list(), list()
 
 n = int(input("Кол-во коньков: "))
@@ -19,3 +22,22 @@ for i in range(len(b)):
         cnt += 1
 
 print("Наибольшее кол-во людей, которые могут взять ролики:", cnt)
+
+#Вариант решения по комментариям
+
+skates, feet = [], []
+
+n = int(input("Кол-во коньков: "))
+for i in range(n):
+    size = int(input(f"Размер {i + 1}-й пары: "))
+    skates.append(size)
+
+people_num = int(input("Кол-во людей: "))
+for i in range(people_num):
+    foot_size = int(input(f"Размер ноги {i + 1}-го человека: "))
+    feet.append(foot_size)
+
+cnt = 0
+for i in range(people_num):
+    if feet[i] in skates:
+        cnt += 1
